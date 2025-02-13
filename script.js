6675149d049ae0dd19617ab419eec69d266eb929
rzik-jpg <gazazikrulrobotic@gmail.com>
2025-02-13 10:21:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Kamu serius?",
+    "Serius endakk mau??",
+    "Positive ndakk mau nemenin?",
+    "Ayoo lahh pleaseee...",
+    "Coba pikirkan lagi manis!",
+    "Kalau no lagi, bakal gila sih aku...",
+    "Sedih bett cokk...",
+    "sedih sedih sedih banget sih ini...",
+    "oke gpp, aku berhenti nanya aja lah...",
+    "bercanda, klik yess pleasee babe! ❤️"
 ];
 
 let messageIndex = 0;
